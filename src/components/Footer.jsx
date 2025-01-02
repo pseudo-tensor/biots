878b81b0078a8dd786e9ba9e56bf78e26581eabe
pseudo-tensor <pkarn125@gmail.com>
2025-01-03 02:24:00 +0530

    fixed team section and footer for firefox

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <div className="min-w-max ms:pt-10 tb:pt-20 lg:pt-10 lg:grid lg:grid-cols-3 items-center justify-items-center bg-neutral-800 text-neutral-50 font-sans">
       <img
         src="biot.png"
-        className="ms:w-28 ms:h-28 tb:w-40 tb:h-40 ms:mb-10 tb:mb-16 lg:my-32 lg:ml-40"
+        className="ms:mx-auto lg:mx-0 ms:w-28 ms:h-28 tb:w-40 tb:h-40 ms:mb-10 tb:mb-16 lg:my-32 ms:mx-auto lg:ml-40"
       />
       <div className="h-fit align-middle lg:pt-4">
         <div className="text-neutral-400 ms:text-sm tb:text-lg">
@@ -44,10 +44,22 @@ function Footer() {
               stroke-linecap="round"
               stroke-linejoin="round"
             >
-              {" "}
-              <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />{" "}
-              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />{" "}
-              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
+              {' '}
+              <rect
+                x="2"
+                y="2"
+                width="20"
+                height="20"
+                rx="5"
+                ry="5"
+              />{' '}
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />{' '}
+              <line
+                x1="17.5"
+                y1="6.5"
+                x2="17.51"
+                y2="6.5"
+              />
             </svg>
           </a>
           <a
@@ -64,10 +76,19 @@ function Footer() {
               stroke-linecap="round"
               stroke-linejoin="round"
             >
-              {" "}
-              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />{" "}
-              <rect x="2" y="9" width="4" height="12" />{" "}
-              <circle cx="4" cy="4" r="2" />
+              {' '}
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />{' '}
+              <rect
+                x="2"
+                y="9"
+                width="4"
+                height="12"
+              />{' '}
+              <circle
+                cx="4"
+                cy="4"
+                r="2"
+              />
             </svg>
           </a>
         </div>
@@ -92,7 +113,7 @@ function Footer() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -108,7 +108,7 @@ function Team() {
         />
       </div>
       <div className=" w-full overflow-hidden text-center ms:my-6 tb:my-8 lg:my-10">
-        <h2 className="ms:text-xl ml:text-2xl tb:text-3xl lg:text-4xl font-bold ms:mb-4 tb:mb-6 lg:mb-8 text-gray-800 relative inline-block">
+        <h2 className="ms:text-xl ml:text-2xl tb:text-3xl lg:text-4xl font-bold ms:mt-4 tb:mt-6 lg:mt-8 text-gray-800 relative inline-block">
           EXECUTIVE MEMBERS
           <span className="block ms:w-30 ms:mt-1 lg:w-32 h-0.5 bg-black ms:mt-2 lg:mt-3 mx-auto"></span>
         </h2>
